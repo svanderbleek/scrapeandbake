943f83b95090e1a9b6ad50a4aa8faa990ddbef95
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2015-05-06 21:24:28 +0700

    Login to InCloak

--- proxy/in_cloak.go
+++ proxy/in_cloak.go
@@ -2,12 +2,23 @@ package proxy
 
 import (
 	"github.com/rentapplication/craigjr/web"
+	"net/http"
+	"net/url"
+	"os"
 	"strings"
 )
 
 const (
-	inCloakList = "http://incloak.com/api/proxylist.txt?out=plain&lang=en"
+	inCloakList  = "http://incloak.com/api/proxylist.txt?out=plain&lang=en"
+	inCloakLogin = "http://incloak.com/login"
 )
+
+type InCloakLoginError struct {
+}
+
+func (icle InCloakLoginError) Error() string {
+	return "InCloak Login Failed"
+}
 
 func inCloak() *Result {
 	result := &Result{}
@@ -21,6 +32,23 @@ func inCloak() *Result {
 }
 
 func inCloakFetch() *web.Query {
+	err := inCloakLoginPost()
+	if err == nil {
+		return inCloakQuery()
+	} else {
+		return &web.Query{Error: err}
+	}
+}
+
+func inCloakLoginPost() error {
+	response, err := http.PostForm(inCloakLogin, url.Values{"c": {os.Getenv("IN_CLOAK_API_KEY")}})
+	if err == nil && response.StatusCode >= 400 {
+		return InCloakLoginError{}
+	}
+	return err
+}
+
+func inCloakQuery() *web.Query {
 	query := &web.Query{
 		Url: inCloakList,
 	}
